e688b59e32f9bfb9dd9c2e5834ea769a7961c014
FIRST_NAME LAST_NAME <FekisCZE@gmail.com>
2021-03-21 15:47:38 +0100

    Metoda1

--- MetodyGit1/Form1.Designer.cs
+++ MetodyGit1/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -65,11 +67,29 @@
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 3;
             // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(403, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Počet dnů:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -87,6 +107,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MetodyGit1/Form1.cs
+++ MetodyGit1/Form1.cs
@@ -17,6 +17,11 @@ namespace MetodyGit1
             InitializeComponent();
         }
 
+        DateTime Metoda1(DateTime datum, int pocetDnu) //Zjistíme datum za zadaný počet dnů
+        {
+            return datum.AddDays(pocetDnu);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             DateTime Narozen1 = DateTime.Parse(textBox1.Text);
@@ -24,10 +29,16 @@ namespace MetodyGit1
             DateTime Narozen3 = DateTime.Parse(textBox3.Text);
             DateTime Nejstarsi = Narozen1;
 
-            if(Narozen2<Nejstarsi)  Nejstarsi = Narozen2;
-            if (Narozen3 < Nejstarsi) Nejstarsi = Narozen3;
+            if(Narozen2<Nejstarsi)  Nejstarsi = Narozen2;       //milostnej, tohle mělo být v metodě, kterou pojmenuješ
+            if (Narozen3 < Nejstarsi) Nejstarsi = Narozen3;     //Nejstarsi a akorát ji tady vyvoláš :)
             MessageBox.Show("Nejstarsi ze tri je: " + Nejstarsi.ToString("dd MM yyy"));
-            
+
+
+            //Metoda1
+            DateTime dnes = DateTime.Today;
+            int x = int.Parse(textBox4.Text);
+
+            MessageBox.Show("Datum po uplynutí " + x + " dnů bude: " + Metoda1(dnes, x).ToShortDateString());
         }
     }
 }
